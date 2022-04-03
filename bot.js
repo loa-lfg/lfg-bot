4c05b205bc0fb94cc3478195ca6df1c2b5e755b9
Eric Lin <eric.lin@nyu.edu>
2022-04-03 18:40:19 -0400

    Refactored for better code structure

--- bot.js
+++ bot.js
@@ -1,17 +1,23 @@
+// Our config file
+const config = require('./config/config.json');
+
+// Discord JS framework
 const { Client, Intents, MessageEmbed } = require('discord.js');
-const config = require('./config.json');
-const discordModals = require('discord-modals')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+// Discord JS Modals add-on
+const discordModals = require('discord-modals');
+discordModals(client);
+// Initialized discord-modals
 
 // create-party.js handles all creation requests and interactions
-const create_party = require('./create-party')
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-// Initialized discord-modals
-discordModals(client);
+const create_party = require('./src/create-party');
+// manage-party.js handles all manage requests for all existing parties
+const manage_party = require('./src/manage-party');
+// logger.js handles all of our file and console logging purposes via winston
+const { logger } = require('./src/logger');
 
 client.once('ready', () => {
-    // Initialize channels
-    console.log(`Lost Ark LFG is ready! Create channel: ${config['create-channel-id']}, List channel ${config['list-channel-id']}`);
+    logger.info(`Lost Ark LFG is ready! Create channel: ${config['create-channel-id']}, List channel ${config['list-channel-id']}`)
 });
 
 // Message processing
